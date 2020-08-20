426384a878013a2de023c409968df5d6f68f8ff8
Dave Lichterman <me@lavid.me>
2020-08-20 16:59:56 -0700

    add GetPlacementFromMIDOIDand DisableDataCheck to common lib

--- src/common.js
+++ src/common.js
@@ -7,3 +7,13 @@ export var AT_LEAST = 1;
 export var EXACT_MATCH = 2;
 export var LEFT_SIDE = 0;
 export var RIGHT_SIDE = 1;
+
+export function GetPlacementFromMIDOID(pi, mid, oid) {
+  var option_placement = pi.modifiers.hasOwnProperty(mid) ?
+    pi.modifiers[mid].find(function (x) { return x[1] === oid }) : null;
+  return option_placement ? option_placement[0] : TOPPING_NONE;
+}
+
+export function DisableDataCheck(disable_data, order_time) {
+  return !disable_data || (!(disable_data.start > disable_data.end) && (disable_data.start > order_time.valueOf() || disable_data.end < order_time().valueOf()));
+}
