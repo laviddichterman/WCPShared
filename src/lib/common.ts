1a7137a8927547189f8c60b679925fd241e8ed9e
Dave Lichterman <me@lavid.me>
2022-08-22 00:12:18 -0700

    v 4.1.8
    * switch common fxns to compute with IMoney

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -1,5 +1,5 @@
 import { getTime } from "date-fns";
-import { CoreCartEntry, DISABLE_REASON, IMoney, IWInterval, ModifiersMap, OptionPlacement, OptionQualifier, TipSelection, ValidateAndLockCreditResponse, WProduct } from "./types";
+import { CoreCartEntry, CURRENCY, DISABLE_REASON, IMoney, IWInterval, ModifiersMap, OptionPlacement, OptionQualifier, TipSelection, ValidateAndLockCreditResponse, WProduct } from "./types";
 
 export const CREDIT_REGEX = /[A-Za-z0-9]{3}-[A-Za-z0-9]{2}-[A-Za-z0-9]{3}-[A-Z0-9]{8}$/;
 
@@ -44,45 +44,45 @@ export function RoundToTwoDecimalPlaces(number: number) {
   return Math.round((number + Number.EPSILON) * 100) / 100;
 }
 
-export function ComputeCartSubTotal(cart: CoreCartEntry<WProduct>[]) {
-  return cart.reduce((acc, entry) => acc + (entry.product.m.price.amount * entry.quantity), 0);
+export function ComputeCartSubTotal(cart: CoreCartEntry<WProduct>[]): IMoney {
+  return { amount: cart.reduce((acc, entry) => acc + (entry.product.m.price.amount * entry.quantity), 0), currency: CURRENCY.USD };
 }
 
-export function ComputeDiscountApplied(subtotalPreDiscount: number, creditValidations: ValidateAndLockCreditResponse[]) {
-  return Math.min(subtotalPreDiscount, creditValidations.reduce((acc, x) => acc + (x.valid && x.credit_type === "DISCOUNT" ? x.amount.amount : 0), 0));
+export function ComputeDiscountApplied(subtotalPreDiscount: IMoney, creditValidations: ValidateAndLockCreditResponse[]): IMoney {
+  return { amount: Math.min(subtotalPreDiscount.amount, creditValidations.reduce((acc, x) => acc + (x.valid && x.credit_type === "DISCOUNT" ? x.amount.amount : 0), 0)), currency: subtotalPreDiscount.currency };
 }
 
-export function ComputeTaxAmount(subtotalAfterDiscount: number, taxRate: number) {
-  return RoundToTwoDecimalPlaces(subtotalAfterDiscount * taxRate);
+export function ComputeTaxAmount(subtotalAfterDiscount: IMoney, taxRate: number): IMoney {
+  return { amount: Math.round(subtotalAfterDiscount.amount * taxRate), currency: subtotalAfterDiscount.currency };
 }
 
-export function ComputeTipBasis(subtotalPreDiscount: number, taxAmount: number) {
-  return subtotalPreDiscount + taxAmount;
+export function ComputeTipBasis(subtotalPreDiscount: IMoney, taxAmount: IMoney): IMoney {
+  return { ...subtotalPreDiscount, amount: subtotalPreDiscount.amount + taxAmount.amount };
 }
 
-export function ComputeTipValue(tip: TipSelection | null, basis: number) {
-  return tip !== null ? (tip.isPercentage ? RoundToTwoDecimalPlaces(tip.value * basis) : tip.value.amount) : 0;
+export function ComputeTipValue(tip: TipSelection | null, basis: IMoney): IMoney {
+  return { currency: basis.currency, amount: tip !== null ? (tip.isPercentage ? Math.round(tip.value * basis.amount) : tip.value.amount) : 0 };
 }
 
-export function ComputeSubtotalPreDiscount(cartTotal: number, serviceFees: number) {
-  return cartTotal + serviceFees;
+export function ComputeSubtotalPreDiscount(cartTotal: IMoney, serviceFees: IMoney): IMoney {
+  return { currency: cartTotal.currency, amount: cartTotal.amount + serviceFees.amount };
 }
 
-export function ComputeSubtotalAfterDiscount(subtotalPreDiscount: number, discountApplied: number) {
-  return subtotalPreDiscount - discountApplied;
+export function ComputeSubtotalAfterDiscount(subtotalPreDiscount: IMoney, discountApplied: IMoney): IMoney {
+  return { currency: subtotalPreDiscount.currency, amount: subtotalPreDiscount.amount - discountApplied.amount };
 }
 
-export function ComputeTotal(subtotalAfterDiscount: number, taxAmount: number, tipAmount: number) {
-  return subtotalAfterDiscount + taxAmount + tipAmount;
+export function ComputeTotal(subtotalAfterDiscount: IMoney, taxAmount: IMoney, tipAmount: IMoney): IMoney {
+  return { currency: subtotalAfterDiscount.currency, amount: subtotalAfterDiscount.amount + taxAmount.amount + tipAmount.amount };
 }
 
-export function ComputeAutogratuityEnabled(mainProductCount: number, threshold: number, isDelivery: boolean) {
+export function ComputeAutogratuityEnabled(mainProductCount: number, threshold: number, isDelivery: boolean): boolean {
   return mainProductCount >= threshold || isDelivery;
 }
 
-export function ComputeGiftCardApplied(total: number, creditValidations: ValidateAndLockCreditResponse[]) {
-  return Math.min(total, creditValidations.reduce((acc, x) => acc + (x.valid && x.credit_type === "MONEY" ? x.amount.amount : 0), 0));
+export function ComputeGiftCardApplied(total: IMoney, creditValidations: ValidateAndLockCreditResponse[]): IMoney {
+  return { currency: total.currency, amount: Math.min(total.amount, creditValidations.reduce((acc, x) => acc + (x.valid && x.credit_type === "MONEY" ? x.amount.amount : 0), 0)) };
 }
-export function ComputeBalanceAfterCredits(total: number, giftCardApplied: number) {
-  return total - giftCardApplied;
+export function ComputeBalanceAfterCredits(total: IMoney, giftCardApplied: IMoney): IMoney {
+  return { currency: total.currency, amount: total.amount - giftCardApplied.amount };
 }
